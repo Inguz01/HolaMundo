206367f52f4b82cda8a0f26a5eef0c71a5ef53b4
Inguz01 <inguz01@gmail.com>
2021-11-16 23:02:43 -0500

    creación del textinputlayaout "estatura" y su respectiva programación en el botón.

--- app/src/main/java/com/inguz01/holamundo/RegistroSuperheroeActivity.kt
+++ app/src/main/java/com/inguz01/holamundo/RegistroSuperheroeActivity.kt
@@ -3,6 +3,7 @@ package com.inguz01.holamundo
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
@@ -16,14 +17,20 @@ class RegistroSuperheroeActivity : AppCompatActivity() {
         val nombreEditText: EditText = findViewById(R.id.nombre_edit_text)
         val infoTextView: TextView =  findViewById(R.id.info_text_view)
         val estaturaEditText: TextInputEditText = findViewById(R.id.estaturaEditText)
-
+        val masculinoradioButton: RadioButton = findViewById(R.id.masculino_radio_Button)
 
         registrarButton.setOnClickListener {
            val nombre : String = nombreEditText.text.toString()
             val estatura : Float = estaturaEditText.text.toString().toFloat()
-            infoTextView.text = getString(R.string.info, nombre, estatura)
+            val genero : String
 
 
+            if (masculinoradioButton.isChecked)
+                genero = getString(R.string.masculino)
+            else
+                genero = getString(R.string.femenino)
+
+            infoTextView.text = getString(R.string.info, nombre, estatura, genero)
 
         }
 
