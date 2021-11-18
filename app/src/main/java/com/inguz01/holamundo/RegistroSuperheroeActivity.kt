9849fc177fd482220bddef604da041809181465e
Inguz01 <inguz01@gmail.com>
2021-11-17 22:39:20 -0500

    creación de tres check Boxes y su respectiva programación en el botón.

--- app/src/main/java/com/inguz01/holamundo/RegistroSuperheroeActivity.kt
+++ app/src/main/java/com/inguz01/holamundo/RegistroSuperheroeActivity.kt
@@ -1,10 +1,7 @@
 package com.inguz01.holamundo
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.RadioButton
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
 
@@ -18,19 +15,28 @@ class RegistroSuperheroeActivity : AppCompatActivity() {
         val infoTextView: TextView =  findViewById(R.id.info_text_view)
         val estaturaEditText: TextInputEditText = findViewById(R.id.estaturaEditText)
         val masculinoradioButton: RadioButton = findViewById(R.id.masculino_radio_Button)
+        val fuerzaCheckBox: CheckBox = findViewById(R.id.Super_Fuerza_Check_Box)
+        val velocidadCheckBox: CheckBox = findViewById(R.id.velocidad_check_box)
+        val telepatiaCheckBox:CheckBox = findViewById(R.id.Telepatia_check_box)
+
 
         registrarButton.setOnClickListener {
            val nombre : String = nombreEditText.text.toString()
             val estatura : Float = estaturaEditText.text.toString().toFloat()
             val genero : String
-
+            var poderes = ""
 
             if (masculinoradioButton.isChecked)
                 genero = getString(R.string.masculino)
             else
                 genero = getString(R.string.femenino)
 
-            infoTextView.text = getString(R.string.info, nombre, estatura, genero)
+
+            if (fuerzaCheckBox.isChecked) poderes = getString(R.string.superfuerza)
+            if (velocidadCheckBox.isChecked) poderes = poderes + " " + getString(R.string.velocidad)
+            if (telepatiaCheckBox.isChecked) poderes = poderes + " " + getString(R.string.telepatia)
+
+            infoTextView.text = getString(R.string.info, nombre, estatura, genero, poderes)
 
         }
 
